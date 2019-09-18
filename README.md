51321c0c539190018fc22de696e0bf71b2b7a670
zhun.yang <zhun.yang@dmall.com>
2019-09-18 15:04:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+## **跳过谷歌验证码**
+
+#### 依赖python3, expect shell
+需要配置好vpn的ssh key
---
+++ dmall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+user="你的vpn账号"
+token="你的谷歌验证token 16位字符串,jumpserver个人信息重置MFA可查看"
+
+SH_HOME=$(pwd)
+code=$(python $SH_HOME/g_code3.py $token)
+expect $SH_HOME/exi $code $user
