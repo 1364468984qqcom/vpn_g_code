d0e325191c79df0ed86578e95c3252620fa9c7ad
zhun.yang <zhun.yang@dmall.com>
2019-09-19 17:41:39 +0800

    python3

--- dmall.sh
+++ dmall.sh
@@ -4,5 +4,5 @@ user="你的vpn账号"
 token="你的谷歌验证token 16位字符串,jumpserver个人信息重置MFA可查看"
 
 SH_HOME=$(cd "$(dirname $0)";pwd)
-code=$(python $SH_HOME/g_code3.py $token)
+code=$(python3 $SH_HOME/g_code3.py $token)
 expect $SH_HOME/vpn_dmall_expect $code $user
