358be68eec6b6650c55978b4190843c49e6794d5
zhun.yang <zhun.yang@dmall.com>
2019-09-24 20:37:22 +0800

    重试10次

--- vpn.sh
+++ vpn.sh
@@ -5,7 +5,10 @@ token="52ZWK75AAF5N3THW"
 jump_server="jump.dmall.com"
 SH_HOME=$(cd "$(dirname $0)";pwd)
 
-for (( i = 0; i < 10; ++i )); do
+for (( i = 0; i <= 10; ++i )); do
+    if [[ $i > 0 ]]; then
+       echo "重试第${i}次"
+    fi
     expect ${SH_HOME}/vpn_expect ${SH_HOME} ${token} ${user} ${jump_server}
     if [[ $? = 0 ]]; then
         exit
