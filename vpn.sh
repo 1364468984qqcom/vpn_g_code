408b2f2141464161d2ed5ac6a689ca0786dde049
zhun.yang <zhun.yang@dmall.com>
2019-09-24 20:32:59 +0800

    xunhuan

--- vpn.sh
+++ vpn.sh
@@ -3,7 +3,13 @@
 user="zhun.yang"
 token="52ZWK75AAF5N3THW"
 jump_server="jump.dmall.com"
-
 SH_HOME=$(cd "$(dirname $0)";pwd)
-code=$(python3 ${SH_HOME}/g_code3.py ${token})
-expect ${SH_HOME}/vpn_expect ${code} ${user} ${jump_server}
+
+for (( i = 0; i < 10; ++i )); do
+    expect ${SH_HOME}/vpn_expect ${SH_HOME} ${token} ${user} ${jump_server}
+    if [[ $? = 0 ]]; then
+        exit
+    fi
+    sleep 1s
+done
+
